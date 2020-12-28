60b4f66708bb78d6a8418d7f70a8144884983db2
feliximhof <imhoffelix@yahoo.de>
2020-12-28 18:17:48 +0100

    added first router implementations

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 
+import { Route, Switch } from "react-router-dom";
+
+function Hats() {
+	return (
+		<div>
+			<h1>lksjadflkjasf</h1>
+		</div>
+	);
+}
+
 function App() {
 	return (
 		<div>
-			<HomePage />
+			<Switch>
+				<Route exact path="/" component={HomePage} />
+				<Route exact path="/hats" component={Hats} />
+			</Switch>
 		</div>
 	);
 }
