47618c0111aeb3ea3d478420de870e578aa6fbba
feliximhof <imhoffelix@yahoo.de>
2021-01-16 11:09:28 +0100

    - customized button more - added button to collection item

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./collection-item.styles.scss";
 
+import CustomButton from "../custom-button/custom-button.component";
+
 const CollectionItem = ({ id, name, price, imageUrl }) => (
 	<div className="collection-item">
 		<div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
@@ -8,6 +10,9 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
 			<span className="name">{name}</span>
 			<span className="price">{price} €</span>
 		</div>
+		<CustomButton inverted={true} className="custom-button">
+			Add to Cart
+		</CustomButton>
 	</div>
 );
 
